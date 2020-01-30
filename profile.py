e37b89ee8d9d2a1c15d7b5d70c6bb31af93bace1
gITWN <55665548+gITWN@users.noreply.github.com>
2020-01-30 14:56:59 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -3,6 +3,10 @@ import geni.rspec.pg as rspec
 
 # Create a Request object to start building the RSpec.
 request = portal.context.makeRequestRSpec()
+
+for i in range(2):
+  
+
 # Create a XenVM
 node = request.XenVM("node")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
